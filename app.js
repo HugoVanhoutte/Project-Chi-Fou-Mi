f755a1a4d29f649979ea660306de66b857626d04
HugoVanhoutte <116795821+HugoVanhoutte@users.noreply.github.com>
2022-12-13 16:38:22 +0100

    Avancé Chi-Fou-Mi     - Retour à la bordure initiale quand clic rapide sur un autre bouton.

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ let computerScore = parseInt(document.getElementById("computer-score").innerText
 function getRandom(){
 
 let random = document.querySelectorAll("#computer-choice .choice")[Math.floor(Math.random()*3)];
+
+document.querySelectorAll("#computer-choice .choice")[0].style.border = "dashed 2px black";
+    document.querySelectorAll("#computer-choice .choice")[1].style.border = "dashed 2px black";
+    document.querySelectorAll("#computer-choice .choice")[2].style.border = "dashed 2px black";
+
 random.style.border = "red 3px solid";
 setTimeout(() => random.style.border = "black 2px dashed", 2000);
 return random.innerText.toLowerCase();
@@ -36,6 +41,8 @@ userStone.addEventListener("click", () => {
     userChoice = "pierre";
     userStone.style.border = "green 3px solid"
     setTimeout(() => userStone.style.border = "dashed 2px black", 2000);
+    userPaper.style.border = "dashed 2px black";
+    userScissors.style.border = "dashed 2px black";
     computerChoice = getRandom();
     getResult(userChoice, computerChoice);
 });
@@ -43,6 +50,8 @@ userPaper.addEventListener("click", () => {
     userChoice = "feuille";
     userPaper.style.border = "green 3px solid"
     setTimeout(() => userPaper.style.border = "dashed 2px black", 2000);
+    userScissors.style.border = "dashed 2px black";
+    userStone.style.border = "dashed 2px black";
     computerChoice = getRandom();
     getResult(userChoice, computerChoice);
 });
@@ -50,6 +59,8 @@ userScissors.addEventListener("click", () =>{
     userChoice = "ciseaux";
     userScissors.style.border = "green 3px solid"
     setTimeout(() => userScissors.style.border = "dashed 2px black", 2000);
+    userStone.style.border = "dashed 2px black";
+    userPaper.style.border = "dashed 2px black";
     computerChoice = getRandom();
     getResult(userChoice, computerChoice);
 });
